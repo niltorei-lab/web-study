03f60dd53a431646bf27722a2096ca30ac1b6c7f
niltorei-lab <niltorei.lab@gmail.com>
2026-04-18 20:43:00 +0900

    学習ログページに一括開閉機能を追加

--- learning-log.js
+++ learning-log.js
@@ -1,14 +1,17 @@
-//20260402のボタンを取得
+//20260402のボタン
 const toggleLog20260402Button = document.getElementById("toggle-log-2026-04-02");
 
-//20260402の学習記録を取得
+//20260402の学習記録
 const logContent20260402 = document.getElementById("log-content-2026-04-02");
 
-//20260409のボタンを取得
+//20260409のボタン
 const toggleLog20260409Button = document.getElementById("toggle-log-2026-04-09");
 
-//20260409の学習記録を取得
+//20260409の学習記録
 const logContent20260409 = document.getElementById("log-content-2026-04-09");
+
+//学習記録の開閉ボタン
+const toggleAllButton = document.getElementById("toggle-all-button");
 
 //20260402の学習記録の表示非表示
 toggleLog20260402Button.addEventListener("click",function(){
@@ -29,5 +32,29 @@ toggleLog20260409Button.addEventListener("click",function(){
     toggleLog20260409Button.textContent = "開く";
   }else{
     toggleLog20260409Button.textContent = "閉じる";
+  }
+});
+
+//学習記録の開閉
+toggleAllButton.addEventListener("click",function(){
+  const allClosed =
+  logContent20260402.classList.contains("hidden") &&
+  logContent20260409.classList.contains("hidden");
+
+  if(allClosed){
+    logContent20260402.classList.remove("hidden");
+    logContent20260409.classList.remove("hidden");
+
+    toggleLog20260402Button.textContent = "閉じる";
+    toggleLog20260409Button.textContent = "閉じる";
+    toggleAllButton.textContent = "すべて閉じる";
+
+  }else{
+    logContent20260402.classList.add("hidden");
+    logContent20260409.classList.add("hidden");
+
+    toggleLog20260402Button.textContent = "開く";
+    toggleLog20260409Button.textContent = "開く";
+    toggleAllButton.textContent = "すべて開く";
   }
 });
